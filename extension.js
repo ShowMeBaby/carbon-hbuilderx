5e6e851b87dcb863df5d6c31dce8981b3075fe7c
retrocode <630413133@qq.com>
2021-01-04 16:02:45 +0800

    1.优化代码结构使之更紧凑

--- extension.js
+++ extension.js
@@ -1,19 +1,12 @@
 'use strict';
 const main = require('./src/main.js');
 const hx = require("hbuilderx");
-//该方法将在插件激活的时候调用
-function activate(context) {
-	let disposable = hx.commands.registerCommand('carbon.openurl', () => {
-    main.carbon();
-	});
-	//订阅销毁钩子，插件禁用的时候，自动注销该command。
-	context.subscriptions.push(disposable);
-}
-//该方法将在插件禁用的时候调用（目前是在插件卸载的时候触发）
-function deactivate() {
 
+function activate(context) {
+  let disposable = hx.commands.registerCommand('carbon.openurl', main.carbon);
+  context.subscriptions.push(disposable);
 }
+
 module.exports = {
-	activate,
-	deactivate
+  activate
 }
--- src/main.js
+++ src/main.js
@@ -9,11 +9,8 @@ function getHbuilderxText() {
     if (!editor) {
       reject('未找到激活的编辑器,操作终止!');
     }
-    const selection = editor.selection;
-    const document = editor.document;
-    const codeWord = document.getText(selection);
-    const languageId = document.languageId;
-
+    const codeWord = editor.document.getText(editor.selection);
+    const languageId = editor.document.languageId;
     if (!codeWord) {
       reject('请先选中需要分享的代码片段!');
     }
@@ -29,50 +26,29 @@ function openUrl(data) {
   const [codeWord, languageId] = data;
   const config = hx.workspace.getConfiguration("carbon-hbuilderx");
   const carbonUrl = new URL(config.get("carbonUrl", 'http://carbon.retrocode.io'));
-  // 背景色支持rgba(默认rgba(0,0,0,0))
-  carbonUrl.searchParams.set("bg", encodeURIComponent(config.get("backgroundColor")));
-
-  // 代码主题(默认vscode)
-  carbonUrl.searchParams.set("t", config.get("theme"));
-  // 代码语言插件自动判断赋值
-  carbonUrl.searchParams.set("l", languageId);
-
-  // 是否显示窗口样式(boolean,默认true)
-  carbonUrl.searchParams.set("wc", config.get("windowControls"));
-  // 窗口主题[none,sharp,bw,boxy]
-  carbonUrl.searchParams.set("wt", config.get("windowTheme"));
-  // 是否显示窗口阴影(boolean)
-  carbonUrl.searchParams.set("ds", config.get("dropShadow"));
-  // 窗口阴影偏移offset-y(0->100)
-  carbonUrl.searchParams.set("dsyoff", config.get("dropShadowOffset")+'px');
-  // 窗口阴影半径blur-radius(0->100)
-  carbonUrl.searchParams.set("dsblur", config.get("dropShadowBlurRadius")+'px');
-
-  // 是否自动调整宽度,即是否换行(boolean)
-  carbonUrl.searchParams.set("wa", config.get("autoAdjustWidth"));
-  // padding上下边距(0->200)
-  carbonUrl.searchParams.set("pv", config.get("paddingVertical")+'px');
-  // padding左右边距(0->100)
-  carbonUrl.searchParams.set("ph", config.get("paddingHorizontal")+'px');
-
-  // 是否显示行数(boolean)
-  carbonUrl.searchParams.set("ln", config.get("lineNumbers"));
-  // 显示行数时的起始行数(0->∞)
-  carbonUrl.searchParams.set("fl", config.get("firstLineNumber"));
-
-  // 字体类型(String,默认'JetBrains Mono')
-  carbonUrl.searchParams.set("fm", config.get("fontFamily"));
-  // 字体大小(10->20)
-  carbonUrl.searchParams.set("fs", config.get("fontSize")+'px');
-  // 行高(90%->250%)
-  carbonUrl.searchParams.set("lh", config.get("lineHeight")+'%');
-
-  // 是否显示carbon水印,默认为false即可
-  carbonUrl.searchParams.set("wm", false);
-
-  // 代码片段
-  carbonUrl.searchParams.set("code", encodeURIComponent(codeWord));
-
+  const carbonSearchParams = {
+    bg: encodeURIComponent(config.get("backgroundColor")), // 背景色支持rgba(默认rgba(0,0,0,0))
+    t: config.get("theme"), // 代码主题(默认vscode)
+    l: languageId, // 代码语言插件自动判断赋值
+    wc: config.get("windowControls"), // 是否显示窗口样式(boolean,默认true)
+    wt: config.get("windowTheme"), // 窗口主题[none,sharp,bw,boxy]
+    ds: config.get("dropShadow"), // 是否显示窗口阴影(boolean)
+    dsyoff: config.get("dropShadowOffset") + 'px', // 窗口阴影偏移offset-y(0->100)
+    dsblur: config.get("dropShadowBlurRadius") + 'px', // 窗口阴影半径blur-radius(0->100)
+    wa: config.get("autoAdjustWidth"), // 是否自动调整宽度,即是否换行(boolean)
+    pv: config.get("paddingVertical") + 'px', // padding上下边距(0->200)
+    ph: config.get("paddingHorizontal") + 'px', // padding左右边距(0->100)
+    ln: config.get("lineNumbers"), // 是否显示行数(boolean)
+    fl: config.get("firstLineNumber"), // 显示行数时的起始行数(0->∞)
+    fm: config.get("fontFamily"), // 字体类型(String,默认'JetBrains Mono')
+    fs: config.get("fontSize") + 'px', // 字体大小(10->20)
+    lh: config.get("lineHeight") + '%', // 行高(90%->250%)
+    wm: false, // 是否显示carbon水印,默认为false即可
+    code: encodeURIComponent(codeWord), // 代码片段
+  };
+  for (let k in carbonSearchParams) {
+    carbonUrl.searchParams.set(k, carbonSearchParams[k])
+  }
   hx.env.openExternal(carbonUrl.toString());
 }
 
